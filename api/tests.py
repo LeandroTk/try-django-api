0e9b365f2f17ffa9d28ca19c37c89d7de66cbb12
Leandro Tk <leandrotk100@gmail.com>
2018-01-01 18:12:39 -0200

    Implemented views to create bucketlist

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,6 @@
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.urls import reverse
 from django.test import TestCase
 from .models import Bucketlist
 
@@ -11,3 +14,15 @@ class ModelTestCase(TestCase):
         self.bucketlist.save()
         new_count = Bucketlist.objects.count()
         self.assertNotEqual(old_count, new_count)
+
+class ViewTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.bucketlist_data = {'name': 'Go to Ibiza'}
+        self.response = self.client.post(
+            reverse('create'),
+            self.bucketlist_data,
+            format='json')
+
+    def test_api_can_create_a_bucketlist(self):
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
